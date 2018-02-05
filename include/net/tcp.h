0b5c482c29ace6947db84fa8b7619c8096a5c70f
John Fastabend <john.fastabend@gmail.com>
2018-02-05 10:17:49 -0800

    UPSTREAM: bpf: sockmap, add sock close() hook to remove socks
    The selftests test_maps program was leaving dangling BPF sockmap
    programs around because not all psock elements were removed from
    the map. The elements in turn hold a reference on the BPF program
    they are attached to causing BPF programs to stay open even after
    test_maps has completed.
    
    The original intent was that sk_state_change() would be called
    when TCP socks went through TCP_CLOSE state. However, because
    socks may be in SOCK_DEAD state or the sock may be a listening
    socket the event is not always triggered.
    
    To resolve this use the ULP infrastructure and register our own
    proto close() handler. This fixes the above case.
    
    Fixes: 174a79ff9515 ("bpf: sockmap with sk redirect support")
    Reported-by: Prashant Bhole <bhole_prashant_q7@lab.ntt.co.jp>
    Change-Id: I828754b5d588f3d9e08735685597f697a35d9a76
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -2093,6 +2093,11 @@ enum hrtimer_restart tcp_pace_kick(struct hrtimer *timer);
 #define TCP_ULP_MAX		128
 #define TCP_ULP_BUF_MAX		(TCP_ULP_NAME_MAX*TCP_ULP_MAX)
 
+enum {
+	TCP_ULP_TLS,
+	TCP_ULP_BPF,
+};
+
 struct tcp_ulp_ops {
 	struct list_head	list;
 
@@ -2107,6 +2112,7 @@ struct tcp_ulp_ops {
 int tcp_register_ulp(struct tcp_ulp_ops *type);
 void tcp_unregister_ulp(struct tcp_ulp_ops *type);
 int tcp_set_ulp(struct sock *sk, const char *name);
+int tcp_set_ulp_id(struct sock *sk, const int ulp);
 void tcp_get_available_ulp(char *buf, size_t len);
 void tcp_cleanup_ulp(struct sock *sk);
 
