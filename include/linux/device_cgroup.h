b8c2a0c48c2fd57050deeaad3ad38f54752ab73b
nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>
2025-10-04 13:53:52 +0000

    include: linux: device_cgroup.h: readd devcgroup_inode_mknod & __devcgroup_inode_permission
     * Removed while cherry-pick by mistake
    
    Change-Id: I63243ecd81b02bfc966ce682ccbefaf3d318de6f
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- include/linux/device_cgroup.h
+++ include/linux/device_cgroup.h
@@ -3,11 +3,17 @@
 #include <linux/bpf-cgroup.h>
 
 #ifdef CONFIG_CGROUP_DEVICE
+extern int __devcgroup_inode_permission(struct inode *inode, int mask);
+extern int devcgroup_inode_mknod(int mode, dev_t dev);
 extern int __devcgroup_check_permission(short type, u32 major, u32 minor,
 					short access);
 #else
 static inline int __devcgroup_check_permission(short type, u32 major, u32 minor,
 					       short access)
+{ return 0; }
+static inline int __devcgroup_inode_permission(struct inode *inode, int mask)
+{ return 0; }
+static inline int devcgroup_inode_mknod(int mode, dev_t dev)
 { return 0; }
 #endif
 
